2362a39405b9b144978b18b699fe7d02ebfe3081
Christopher Kennedy <chris94kennedy@gmail.com>
2019-07-25 20:21:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,6 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = 
+programmer_hash[:dennis_ritchie][:languages] = [:languages].push("Assembly")
 return programmer_hash
 end
